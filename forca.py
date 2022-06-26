1a376299169bcf5ec78741d067a64c2791a6bbef
cspinola <54821284+cspinola@users.noreply.github.com>
2022-06-26 10:51:52 -0300

    Melhoramento do jogo
    Incluído mais palavras e opção de continuar jogando ou não.

---
+++ forca.py
@@ -0,0 +1,63 @@
+import random
+
+print('******************************************')
+print('***Bem	vindo	ao	jogo	da	Forca!***')
+print('******************************************')
+
+jogar = True
+
+while jogar:
+    plavras = ['banana', 'palavra', 'escova',
+               'chave', 'caneta', 'prato', 'papel', 'comida']
+    palavra_secreta = random.choice(plavras)
+    # palavra_secreta = 'banana'
+
+    acertou = False
+    enforcou = False
+
+    letras_acertadas = []
+    for i in palavra_secreta:
+        letras_acertadas.append('_')
+
+    erros = 0
+
+    print('Tente acertar a Palavra!\n Você possui 6 tentativas no caso a letra digitada não faça parte da palavra secreta.')
+
+    while (not acertou and not enforcou):
+        chute = input('Digite uma letra:')
+
+        if (chute in palavra_secreta):
+
+            posicao = 0
+
+            for letra in palavra_secreta:
+                if (chute.upper() == letra.upper()):
+                    print('Encontrei a letra {} na posição {}'.format(
+                        letra, posicao))
+                    letras_acertadas[posicao] = letra
+                posicao += 1
+
+        else:
+            erros += 1
+            print('Essa letra não faz parte da palavra secreta, você tem {} tentativas'.format(
+                6-erros))
+
+        acertou = '_' not in letras_acertadas
+        enforcou = erros == 6
+        print(letras_acertadas)
+    # print('Você errou {} sendo o máximo de 6'.format(erros))
+
+    if (acertou):
+        print('Parabéns! Você acertou! A palavra secreta era {}.'.format(
+            palavra_secreta))
+    else:
+        print('Você perdeu o jogo! A palavra secreta era {}.'.format(palavra_secreta))
+
+    continuar = input("Deseja continuar jogando? (s/n)")
+
+    if continuar == 's':
+        jogar = True
+    else:
+        jogar = False
+
+print('Fim do Jogo!!!')
--- teste2.py
+++ teste2.py
@@ -1,2 +1,23 @@
-for rodada in range(1, 10):
-    print(rodada)
+
+import random
+
+plavras = ['banana', 'palavra', 'escova', 'azul',
+           'chave', 'caneta', 'prato', 'papel', 'comida']
+jogar = True
+
+
+def selectRandom(plavras):
+    return random.choice(plavras)
+
+
+palavra_secreta = selectRandom(plavras)  # random.choice(plavras)
+
+letras_acertadas = []
+for i in palavra_secreta:
+    letras_acertadas.append('_')
+
+#print("The name selected is: ", selectRandom(plavras))
+print(palavra_secreta)
+print((palavra_secreta.__sizeof__()))
+
+print(letras_acertadas)
